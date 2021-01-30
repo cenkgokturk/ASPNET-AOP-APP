322b5efc93bd8578a95b0a639382175f0e760932
Cenk Gokturk <cenkgokturk06@gmail.com>
2021-01-30 16:20:06 +0300

    User authentication has been integrated

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AOPSampleApp.Areas.Identity.Data;
+using AOPSampleApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AOPSampleApp.Areas.Identity.IdentityHostingStartup))]
+namespace AOPSampleApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AOPContent>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AOPContentConnection")));
+
+                services.AddDefaultIdentity<AOPUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<AOPContent>();
+            });
+        }
+    }
+}
